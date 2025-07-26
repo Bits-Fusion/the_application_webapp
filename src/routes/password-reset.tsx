6e3f98f129c606cc92734bc067ab0d98461a6868
unknown <workumyfather@gmail.com>
2025-07-26 16:07:44 +0300

    feat: adding shadcn admin template

--- src/routes/password-reset.tsx
+++
@@ -1,6 +0,0 @@
-import ForgotPassword from "@/components/auth/password-reset";
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/password-reset")({
-  component: ForgotPassword,
-});
